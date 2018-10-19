c6fb63f26d9b1eb798422d62476713f9529a1b51
LAPTOP-INNV7NA1\Game Taiko <zainal@gametaiko.com>
2018-10-19 16:25:00 +0800

    Main menu completed

--- Main/LampuSentuh.cs
+++ Main/LampuSentuh.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class LampuSentuh : MonoBehaviour {
-    
-    private void OnTriggerEnter(Collider other)
+
+    public Transform hand;
+    private void Update()
     {
-        SceneManager.LoadScene("Taman");
+        if(Vector3.Distance(hand.position, transform.position) < 1f)
+        {
+            Debug.Log("Move ke taman");
+        }
     }
 }
