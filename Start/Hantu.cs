b74368524e8dfbeff3232c2da99d418520575e81
Zainal <zainal@gametaiko.com>
2018-10-21 11:37:21 +0800

    Hantu script

---
+++ Start/Hantu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hantu : MonoBehaviour {
+
+
+    public AudioSource[] audiosource;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void PlaySound(AudioClip ac)
+    {
+        GetComponent<AudioSource>().PlayOneShot(ac);
+    }
+
+    public void PlaySoundLocal(int index)
+    {
+        audiosource[index].Play();
+    }
+}
