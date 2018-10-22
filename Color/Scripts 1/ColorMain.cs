0ffa8488613fd623e84d6d224f67a88a90e55975
mehyme <ahtimhanim@gmail.com>
2018-10-22 10:49:32 +0800

    Script, Materials, 3D Models and Prefabs for Color Match Game

---
+++ Color/Scripts 1/ColorMain.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ColorMain : MonoBehaviour {
+
+    public UnityEvent OnClick = new UnityEvent();
+    public GameObject thisColor;
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Color")
+        {
+            print("Clicked");
+            OnClick.Invoke();
+        }
+    }
+
+    //void Update()
+    //{
+    //    var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    //    RaycastHit mHit;
+
+    //    if (Input.GetMouseButton(0) && Physics.Raycast(ray, out mHit) && mHit.collider.gameObject == gameObject && gameObject.tag == "Color")
+    //    {
+    //        print("Clicked");
+    //        OnClick.Invoke();
+    //    }
+    //}
+}
---
+++ Color/Scripts 1/ColorQuestion.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorQuestion : MonoBehaviour {
+
+    //for randomize the question
+    public GameObject hint1;
+    public GameObject hint2;
+    public GameObject hint3;
+    
+    [HideInInspector]
+    public static int questionIndex;
+
+    void Start()
+    {
+        Question();
+    }
+
+    public void Question()
+    {
+        switch (Random.Range(0,3))
+        {
+            case 0:
+                //magenta (R,B)
+                questionIndex = 0;
+                hint1.SetActive(true);
+                hint2.SetActive(false);
+                hint3.SetActive(false);
+                print("Q "+ questionIndex);
+                break;
+
+            case 1:
+                //cyan (B,G)
+                questionIndex = 1;
+                hint2.SetActive(true);
+                hint1.SetActive(false);
+                hint3.SetActive(false);
+                print("Q"+ questionIndex);
+                break;
+
+            default:
+                //yellow (R,G)
+                questionIndex = 2;
+                hint3.SetActive(true);
+                hint1.SetActive(false);
+                hint2.SetActive(false);
+                print("Q"+ questionIndex);
+                break;
+
+        }
+    }
+}
