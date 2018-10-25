9d5c7bd554a385403922042faf9345af74764738
mehyme <ahtimhanim@gmail.com>
2018-10-25 10:34:19 +0800

    Update Gamelan (Scripting functions, Nenek Kebayan model)

---
+++ Gamelan/Script/Gamelan.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Gamelan : MonoBehaviour {
+
+    public UnityEvent OnClick = new UnityEvent();
+    public GameObject definedButton;
+    public AudioSource gamelanSound;
+
+    // Use this for initialization
+    void Start()
+    {
+        definedButton = this.gameObject;
+    }
+
+    private void OnMouseDown()
+    {
+        gamelanSound.Play();
+        Debug.Log("Button Clicked");
+        OnClick.Invoke();
+    }
+}
---
+++ Gamelan/Script/GamelanHit.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamelanHit : MonoBehaviour {
+
+    public List<int> WhichGamelan = new List<int>();
+    public List<int> MusicSheet = new List<int>();
+
+    public void GetGamelan(int hit)
+    {
+        WhichGamelan.Add(hit);
+        CompareList();
+    }
+
+    //call this everytime hit gamelan
+    public bool CompareList()
+    {
+        for(int i=0; i<WhichGamelan.Count; i++)
+        {
+            if(WhichGamelan[i] != MusicSheet[i])
+            {
+                print("Lose");
+                return false;
+            }
+        }
+        return true;
+    }
+}
