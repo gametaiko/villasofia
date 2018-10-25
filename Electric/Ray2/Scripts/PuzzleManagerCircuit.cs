6fb2b1a74b2e0d573665328d4649aaed93466412
LAPTOP-2ARB8NUQ\funky <raymond.lwl89@gmail.com>
2018-10-25 21:33:36 +0800

    Update fusebox

---
+++ Electric/Ray2/Scripts/PuzzleManagerCircuit.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PuzzleManagerCircuit : MonoBehaviour {
+
+    string Name1, Name2, color1, color2;
+    public GameObject[] test;
+    float delay = 3.0f;
+
+    private bool hasFailed = false;
+
+    bool wireRed, wireBlue, wireBlack = false;
+	
+	void Start () {
+        Name1 = "";
+        color1 = "";
+	}
+	
+    private void OnTriggerEnter(Collider other)
+    {
+        if(Name1 == "" && color1 == "")
+        {
+            Name1 = other.gameObject.name;
+            color1 = other.gameObject.tag;
+        }
+        else
+        {
+            Name2 = other.gameObject.name;
+            color2 = other.gameObject.tag;
+            CheckIfMatch();
+        }
+
+        print(Name1 + color1);
+        print(Name2 + color2);
+        
+    }
+
+    void CheckIfMatch()
+    {
+        if(Name1 != Name2 && color1 == color2)
+        {
+            DisableCollider();
+            print("Match!");
+            Name1 = "";
+            color1 = "";
+            Name2 = "";
+            color2 = "";
+
+        }      
+        else if (Name1 == Name2 && color1 == color2)
+        {
+            print("Same circuit, dumbass.");
+            Name1 = "";
+            color1 = "";
+            Name2 = "";
+            color2 = "";
+            //dull electric sound or something
+        }
+        else if(color1 != color2)
+        {
+            print("Does not match.");
+            Name1 = "";
+            color1 = "";
+            Name2 = "";
+            color2 = "";
+
+            //Fail condition
+            hasFailed = true;
+        
+        }
+    }
+
+    void DisableCollider()
+    {
+
+        if (color1 == "Red" && color2 == "Red")
+        {
+            test = GameObject.FindGameObjectsWithTag("Red");
+            foreach (GameObject sample in test)
+            {
+                sample.GetComponent<SphereCollider>().enabled = false;
+            }
+
+            GameObject CircuitRed = GameObject.Find("WireRed");
+            CircuitRed.GetComponent<Renderer>().enabled = true;
+            wireRed = true;
+        }
+
+        else if (color1 == "Blue" && color2 == "Blue")
+        {
+            test = GameObject.FindGameObjectsWithTag("Blue");
+            foreach (GameObject sample in test)
+            {
+                sample.GetComponent<SphereCollider>().enabled = false;
+            }
+
+            GameObject CircuitBlue = GameObject.Find("WireBlue");
+            CircuitBlue.GetComponent<Renderer>().enabled = true;
+            wireBlue = true;
+        }
+
+        else if (color1 == "Black" && color2 == "Black")
+        {
+            test = GameObject.FindGameObjectsWithTag("Black");
+            foreach (GameObject sample in test)
+            {
+                sample.GetComponent<SphereCollider>().enabled = false;
+            }
+
+            GameObject CircuitBlack = GameObject.Find("WireBlack");
+            CircuitBlack.GetComponent<Renderer>().enabled = true;
+            wireBlack = true;
+        }
+    }
+
+
+    private void Update()
+    {
+        if(wireRed && wireBlue && wireBlack == true)
+        {
+            print("You win!");
+            // StartCoroutine(TransitionCountdown(delay));
+        }
+    }
+
+
+    IEnumerator TransitionCountdown(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        //SceneManager.LoadScene("");
+    }
+
+    void PlayVoice()
+    {
+
+    }
+}
---
+++ Pipe/Ray/Scripts/PuzzleManagerPipes.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class PuzzleManagerPipes : MonoBehaviour {
+    Valve4Rotate any;
+    Valve4Rotate valve;
+    float delay = 3f;
+
+    void Start ()
+    {
+        GameObject valve4 = GameObject.Find("polySurface20");
+        valve = valve4.GetComponent<Valve4Rotate>();
+    }
+
+
+    private void Update()
+    {
+        if(valve.position == "left")
+        {
+            print("You win!");
+           // StartCoroutine(TransitionCountdown(delay));
+        }
+    }
+
+
+    IEnumerator TransitionCountdown(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        //SceneManager.LoadScene("")
+    }
+}
